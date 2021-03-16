139219b038b46b4253216ac9d5404ee8eb7b8bcd
Imane-C <imane.choukhii@hotmail.fr>
2021-03-16 17:41:51 +0100

    print énoncé du quiz

--- View/exercice.php
+++ View/exercice.php
@@ -3,39 +3,42 @@
 	<div id=exercice>
 
 		<?php 
-		$quizId = 0;//Numéro du quizz dépend de la page cliquée (variable à mettre)
-		$bddName = 'db_project';				//Nom de la base de donnée en jeu aussi
-		?>
-
-		<h1><?php echo "Quiz ".$quizId; ?></h1> 
-		<br> 
-		<?php //Ajouter la description du quizz après
 		try{ 			 
-			$con = new PDO('mysql:host=localhost;dbname='.$bddName,'root','');  //Connection PDO à bdd à faire extérieurement
+			$con = new PDO('mysql:host=localhost;dbname=db_project','root','');  //Connection PDO à bdd à faire extérieurement
 		}catch(PDOException $e){ 
 				die('Erreur : '.$e->getMessage()); 
 		} 
 		?>
+
+		<?php //display quizz info
+		$quizId=0;
+		$quizSelection= $con->query("SELECT `quizz_id`, `quizz_name`,`quizz_difficulty`, `quizz_description` FROM `quizz` WHERE `quizz_id`= $quizId")->fetch();
+		?>
+
+		<h1><?php echo $quizSelection['quizz_name']; ?></h1>
+
+		<br> 
+		<?php echo "Diffuculté: ".$quizSelection['quizz_difficulty']; ?>
+		<br> 
+		<?php echo "Enoncé: ".$quizSelection['quizz_description']; ?>
 
 	</div>
 
 	<div>
 		<h3>Answer All Questions : </h3>
 
-		<?php    
-		$quizSelection= $con->query("SELECT `question_id`, `question_text`,`question_answer` FROM `question` WHERE `quizz_id`= $quizId");
+		<?php //display questions   
+		$questionSelection= $con->query("SELECT `question_id`, `question_text`,`question_answer` FROM `question` WHERE `quizz_id`= $quizId");
 
- 		foreach ($quizSelection as $question) {
+ 		foreach ($questionSelection as $question) {
   			$questionText=$question['question_text'];
   			echo $questionText;
   			?>
-  			<br>
+  			<br> 
   			<input type="text" id="<?php echo($question['question_id']);?>" name="<?php echo($question['question_id']);?>">
   			<br>
   		<?php
-  		}
-
-		$quizSelection->closeCursor(); 
+  		} 
 		?>
 
 		<br>
@@ -43,11 +46,11 @@
 			<input type="submit" name="submitAnswers" value="Submit">
 		</form>
 
-      	<?php //ecrire les données réponse user
+      	<?php //write user answers
       	if(isset($_POST['submit'])){
 			//$submitAnswers= $con->query("INSERT INTO user_answer(user_text) VALUES (1)"); ------> INSERT DATETIME, USERID LINK, USER INPUT VALUE
 		}
-		$submitAnswers->closeCursor(); 
+		//$submitAnswers->closeCursor(); 
       	?>
 
 	</div>
