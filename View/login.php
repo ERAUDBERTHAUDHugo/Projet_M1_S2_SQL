f123d46e5d2cd09f1da754234c9a547777307d7d
Thibault - Goedert <thibault.goedert@student.junia.com>
2021-03-17 10:32:43 +0100

    Design loginPage

--- View/login.php
+++ View/login.php
@@ -1,3 +1,14 @@
-<div id=login>
-    <p> Vous etes sur la page LOGIN</p>
+<div class="loginbox">
+<link rel="stylesheet" type="text/css" href="styleLogin.css">
+<img src="avatar.png" class="avatar">
+    <h1>Connecte-toi ici !</h1>
+    <form>
+        <p>Pseudo</p>
+        <input type="text" name="" placeholder="Entre ton pseudo">
+        <p>Mot de passe</p>
+        <input type="password" name="" placeholder="Entre ton mot de passe">
+        <input type="submit" name="" value="Se connecter">
+        <a href="#">Lost your password ?</a><br>
+        <a href="#">Don't have an account ?</a>
 </div>
+
