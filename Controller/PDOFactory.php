c7437b4810890f010b875d37132bbeb8c3c507db
ERAUD-BERTHAUD <hugo.eraud-berthaud@isen.yncrea.fr>
2021-03-17 14:24:16 +0100

    bdd fixed

--- Controller/PDOFactory.php
+++ Controller/PDOFactory.php
@@ -4,9 +4,9 @@
 		private static $connexion;
 			public static function get(){
 			   if(!self::$connexion instanceof PDO){
-			      self::$connexion = new PDO('mysql:host=localhost;dbname=sqlprojet;charset=utf8', 'root', '');
+			      self::$connexion = new PDO('mysql:host=localhost;dbname=db_project;charset=utf8', 'root', '');
 			   }
 			   return self::$connexion;
 			}
 	}
-?>
+
