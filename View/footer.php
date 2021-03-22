956c938927eb4af572faa58d9878b1be21b45219
Thibault - Goedert <thibault.goedert@student.junia.com>
2021-03-22 10:41:41 +0100

    Auto stash before merge of "baseSite" and "origin/baseSite"
    Make footer

--- View/footer.php
+++ View/footer.php
@@ -1,4 +1,19 @@
+<<<<<<< Updated upstream
 <footer id="footerContent"> 
 	<a  class="navlink icon" id="iconMail" href=""><i class="icon fas fa-envelope"></i>Contact us </a>
 	<a class="navlink icon" id="aboutUs" href="index.php?page=aboutUs"><i class="fas fa-user-friends"></i>About Us</a>
 </footer>
+=======
+<footer class="footer">
+<link rel="stylesheet" type="text/css" href="View/styleFooter.css">
+	<div class="footer-content">
+		 <h3>A propos de nous</h3>
+		 <p>Nous sommes une équipe de 4 personnes constituée de Imane Choukhi, Hugo Eraud-Berthaud, Erwin Tchayem et Thibault Goedert. Ce site a été crée à l'occcasion de notre projet M1S2.</p>
+		 <a  class="navlink icon" id="iconMail" href="mailto:hugo.eraud-berthaud@isen.yncrea.fr;johan.roux@isen.yncrea.fr;pierric.charbonnier@isen.yncrea.fr;benjamin.goosens@isen.yncrea.fr;alex.capoen@isen.yncrea.fr">
+			 <i class="icon fas fa-envelope"></i>Nous contacter</a>
+	</div>
+</footer>
+
+
+
+>>>>>>> Stashed changes
