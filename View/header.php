da9720a47ef0372ca2717a5e75b0753bb30a7ecb
ERAUD-BERTHAUD <hugo.eraud-berthaud@isen.yncrea.fr>
2021-03-22 10:31:34 +0100

    Correction des bugs

--- View/header.php
+++ View/header.php
@@ -1,4 +1,5 @@
 <header>
+	<link rel="stylesheet" type="text/css" href="View/styleHeader.css">	
     <h1>Apprendre la BDD avec DD</h1>
     <ul id="menu-demo2">
 		<li><a href="index.php?page=main">Accueil</a>
@@ -8,7 +9,7 @@
 		<li><a href="index.php?page=dashboard"> Tableau d'exercices </a>
 		</li>
 	</ul>
-<h6><a href ="index.php?page=login"><button>Log out</button> </h6> 
+<h6><a href ="index.php?page=login"><button>Log out</button></a> </h6> 
 
 </header>	
 
