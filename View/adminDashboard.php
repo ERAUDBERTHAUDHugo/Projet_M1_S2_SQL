7dd848fa04cabec31713a945014002e4910c5849
Imane-C <imane.choukhii@hotmail.fr>
2021-03-31 20:23:11 +0200

    correction display page

--- View/adminDashboard.php
+++ View/adminDashboard.php
@@ -1,13 +1,5 @@
     <?php
-    class BDD{
-        private static $connexion;
-            public static function get(){
-               if(!self::$connexion instanceof PDO){
-                  self::$connexion = new PDO('mysql:host=localhost;dbname=db_project;charset=utf8', 'root', '');
-               }
-               return self::$connexion;
-            }
-    }
+
 
     function displayStudents($teamId,$groupId){ //id of group selected 
         //get team-group name
