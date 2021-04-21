45b1649b3c60485d67dc5777c54bf04381ed91e0
Imane-C <imane.choukhii@hotmail.fr>
2021-04-21 11:35:21 +0200

    manage case no ex in progress

--- View/exercice.php
+++ View/exercice.php
@@ -33,41 +33,82 @@
 			$_SESSION["question"]=$_SESSION["question"]+1;
 		}elseif(isset($_POST["previous"]) and ($_SESSION["question"]>0)){
 			$_SESSION["question"]=$_SESSION["question"]-1;
-		}	
-	}
-	displayIntro();
+		}
+		displayIntro();		
 
-	if (isset($_POST["reponse"])){
-		
-		include("Controller/correction.php");
+		if (isset($_POST["reponse"])){
+			
+			include("Controller/correction.php");
 
-		$userId=$_SESSION["user"]; //get user last name for hash
-		$username=BDD::get()->query("SELECT `user_last_name` FROM `users` WHERE `user_id`= $userId")->fetchAll(); 
-		$dbname=hash("MD5",$username[0]["user_last_name"]);
-		$dbnameCorrec=$dbname."Correc";
-		
-		$getQuestionId=$_SESSION["question"];//get actual question id
-		$questionId=BDD::get()->query("SELECT `question_id` FROM `question` WHERE `question_id`= $getQuestionId")->fetchAll();
-		var_dump($questionId);
-		$Id=$questionId[0][0];
+			$userId=$_SESSION["user"]; //get user last name for hash
+			$username=BDD::get()->query("SELECT `user_last_name` FROM `users` WHERE `user_id`= $userId")->fetchAll(); 
+			$dbname=hash("MD5",$username[0]["user_last_name"]);
+			$dbnameCorrec=$dbname."Correc";
+			
+			$getQuestionId=$_SESSION["question"];//get actual question id
+			$questionId=BDD::get()->query("SELECT `question_id` FROM `question` WHERE `question_id`= $getQuestionId")->fetchAll();
+			var_dump($questionId);
+			$Id=$questionId[0][0];
 
-		$resultat=dataBaseComparision($dbname,$dbnameCorrec,$_POST["reponse"],$_GET['id'],$Id); //call function correction
-		echo " Résultat: ".$resultat[0]." - Points:".$resultat[1]." - Valide: ".$resultat[2];
-		////////////////////////////////////test insertion user answer///////////////////////////////////////////
-		writeUserAnswer($resultat[0],(int)$Id,(int)$userId,(int)$resultat[1], (int)$resultat[2], (int)$_GET['id']); //write user answer in database
+			$resultat=dataBaseComparision($dbname,$dbnameCorrec,$_POST["reponse"],$_GET['id'],$Id); //call function correction
+			echo " Résultat: ".$resultat[0]." - Points:".$resultat[1]." - Valide: ".$resultat[2];
+			////////////////////////////////////test insertion user answer///////////////////////////////////////////
+			writeUserAnswer($resultat[0],(int)$Id,(int)$userId,(int)$resultat[1], (int)$resultat[2], (int)$_GET['id']); //write user answer in database
 
-		/////////////////////////////////////////////////////////////////////////////////////////////////////////
-		?>
-		<form action="index.php?page=exercice&id=<?php echo($_GET["id"]);?>" method="POST">
-			<button name="previous">Question précédente</button>
-		</form>
-		<form action="index.php?page=exercice&id=<?php echo($_GET["id"]);?>" method="POST">
-			<button name="next">Question suivante</button>
-		</form>
+			/////////////////////////////////////////////////////////////////////////////////////////////////////////
+			?>
+			<form action="index.php?page=exercice&id=<?php echo($_GET["id"]);?>" method="POST">
+				<button name="previous">Question précédente</button>
+			</form>
+			<form action="index.php?page=exercice&id=<?php echo($_GET["id"]);?>" method="POST">
+				<button name="next">Question suivante</button>
+			</form>
 
-		<?php
+	<?php
+		}else{
+			displayQuestion();
+		}
 	}else{
-		displayQuestion();
+	?>
+	<p>Aucun exercice en cours</p>
+	<?php
+
+		$userId1 = $_SESSION['user'];
+
+        $tpNames=BDD::get()->query("SELECT tp_name FROM tp WHERE equipe_id IN (SELECT equipe_id FROM groupe WHERE groupe_id IN (SELECT groupe_id FROM part_of WHERE user_id=$userId1))")->fetchAll();
+
+        $exerciseInfos=BDD::get()->query("SELECT quiz_id, quiz_name,quiz_difficulty, quiz_description, user_id FROM quiz WHERE quiz_id IN (SELECT quiz_id FROM tp WHERE equipe_id IN (SELECT equipe_id FROM groupe WHERE groupe_id IN (SELECT groupe_id FROM part_of WHERE user_id=$userId1)))")->fetchAll();
+    ?>
+    <h1>Sélectionnez un exercice</h1>
+    <div class="quiz-row">
+        <?php 
+            $index=0;
+            foreach ($exerciseInfos as $infos) { //afficher tous les exercices disponibles
+                ?>
+                <a href="index.php?page=exercice&id=<?php echo($infos["quiz_id"]); ?>">
+                <div class="quiz-box">
+
+                    <?php //recuperer le teacher qui a crée le quiz
+                    $id=(int)$exerciseInfos[$index]['user_id'];
+                    $quizCreator=BDD::get()->query("SELECT `user_id`,`user_last_name`,`user_first_name` FROM `users` WHERE `user_id`=$id")->fetchAll();
+                    ?>
+
+                    <h4><?php echo $tpNames[$index]['tp_name']; ?></h4>
+                    <small><?php echo "Par ".$quizCreator[0]['user_first_name']." ".$quizCreator[0]['user_last_name'];?></small>
+                    <img src="View/Img/avatar.png">
+                    <div class="title-quiz-box">
+                        <p1><?php echo $exerciseInfos[$index]['quiz_name']; ?></p1>
+                    </div>
+                    <p><?php echo substr($exerciseInfos[$index]['quiz_description'], 0, 55)."..."; ?></p>
+                </div>
+                </a>
+                <?php  
+                $index+=1;      
+            }
+         ?>
+    </div>
+
+    <?php
 	}
 	?>
 
